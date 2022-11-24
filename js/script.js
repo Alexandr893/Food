e97a57ff26d61cab808ef90b8832fca0801769d0
alexandr <Alexandr6732@mail.ru>
2022-11-24 14:58:39 +0700

    timer

--- js/script.js
+++ js/script.js
@@ -36,18 +36,64 @@ const tabs = document.querySelectorAll('.tabheader__item'),
 
 
 
-            })
+            });
         }
 
     });
 
+    // timer
+
+    const deadLine = '2022-12-24';
+
+    function getTime(endTime){
+        const t= Date.parse(endTime)- Date.parse(new Date());
+        // количество дней до конца
+        days = Math.floor(t/(1000*60*60*24));
+        hours = Math.floor((t/(1000*60*60)) % 24 );
+        minutes = Math.floor((t/(1000/60))% 60);
+        seconds = Math.floor((t/1000)%60);
+
+        
+        return {
+            total:t,
+            days: days,
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds
+        };
+    }
+
+    // рендер
+
+    function setClock(selector,endTime){
+        // на вход родитель .timer
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds');
+              timeInterval = setInterval(updateClock,1000);
+            updateClock();
+
+              function updateClock(){
+                const t = getTime(endTime);
+
+                days.innerHTML = t.days;
+                hours.innerHTML = t.hours;
+                minutes.innerHTML = t. minutes;
+                seconds.innerHTML = t.seconds;
+// total ушел в минус
+                if(t.total <=0){
+                    clearInterval(timeInterval);
+                }
+              }
+    }
 
 
 
 
 
-
-
+    setClock('.timer',deadLine);
 
 
 
