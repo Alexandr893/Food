e7301be8157b8fcbd5b11b75b916455ae97cbc67
alexandr <Alexandr6732@mail.ru>
2022-11-27 01:25:03 +0700

    rest operator

--- js/script.js
+++ js/script.js
@@ -128,25 +128,27 @@ modalCloseBtn.addEventListener('click',(closeModal));
 
 
     class Menucard {
-        constructor(src,alt,title,descr,price,parentSelector){
+        constructor(src,alt,title,descr,price,parentSelector,  ...classes){
             this.src = src;
             this.alt= alt;
             this.title = title;
             this.descr = descr;
             // вернет уже измененное значение
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 27;
             this.ChangeToСurrency();
         }
 // конвертация валюты
         ChangeToСurrency(){
-            this.price = this.price = this.transfer;
+            this.price = this.price * this.transfer;
         }
 
         render(){
             const element = document.createElement('div');
-            element.innerHTML = `<div class="menu__item">
+            this.classes.forEach(className =>element.classList.add(className));
+            element.innerHTML = `
             <img src= ${this.src} alt=${this.alt}>
             <h3 class="menu__item-subtitle">${this.title}</h3>
             <div class="menu__item-descr">${this.descr}</div>
@@ -155,7 +157,7 @@ modalCloseBtn.addEventListener('click',(closeModal));
                 <div class="menu__item-cost">Цена:</div>
                 <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
             </div>
-        </div>
+        
 
             `;
             this.parent.append(element);
@@ -171,7 +173,8 @@ modalCloseBtn.addEventListener('click',(closeModal));
         'Меню "Фитнес"',
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     ).render();
 
     new Menucard(
@@ -180,7 +183,8 @@ modalCloseBtn.addEventListener('click',(closeModal));
         'Меню "Фитнес"',
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     ).render();
     
     new Menucard(
@@ -189,7 +193,8 @@ modalCloseBtn.addEventListener('click',(closeModal));
         'Меню "Фитнес"',
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9,
-        '.menu .container'
+        '.menu .container',
+'menu__item'
     ).render();
 
 });
