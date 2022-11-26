403df494b9734212338858aa48b5eea2edda47fd
alexandr <Alexandr6732@mail.ru>
2022-11-27 01:02:00 +0700

    class

--- js/script.js
+++ js/script.js
@@ -124,9 +124,73 @@ modalCloseBtn.addEventListener('click',(closeModal));
         });
 
     //modal inervav
-    const modalTimer = setTimeout(openModal,2000);
+    // const modalTimer = setTimeout(openModal,2000);
 
 
+    class Menucard {
+        constructor(src,alt,title,descr,price,parentSelector){
+            this.src = src;
+            this.alt= alt;
+            this.title = title;
+            this.descr = descr;
+            // вернет уже измененное значение
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.ChangeToСurrency();
+        }
+// конвертация валюты
+        ChangeToСurrency(){
+            this.price = this.price = this.transfer;
+        }
+
+        render(){
+            const element = document.createElement('div');
+            element.innerHTML = `<div class="menu__item">
+            <img src= ${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>
+        </div>
+
+            `;
+            this.parent.append(element);
+        }
+    }
+
+
+
+    new Menucard(
+        // на вход
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'
+    ).render();
+
+    new Menucard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'
+    ).render();
+    
+    new Menucard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'
+    ).render();
 
 });
 
